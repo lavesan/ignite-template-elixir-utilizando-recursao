80caaf9ff3bf9169a810660e670ee7b691a5fcd5
Valdery Alves <valdery.jur@gmail.com>
2022-04-24 15:34:13 -0300

    feat: adds function to get length of list
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: getLength(list, 0)
+
+  defp getLength([], length), do: length
+
+  defp getLength([_ | tail], length) do
+    length = length + 1
+    getLength(tail, length)
   end
 end
